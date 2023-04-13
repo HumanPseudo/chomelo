d9349be517c83da7e5fdc9e3b6bfcb20e7b3b64c
David Lasso <camilolasso@trascenderglobal.com>
2023-04-13 17:16:24 -0500

    correcciones

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -25,11 +25,52 @@ const Cafe = () => {
 
   return (
     <section className="bg-primary text-white px-5 py-32" id="projects">
-      <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+    
+      <div className="container mx-auto grid md:grid-cols-1 items-center md:justify-between">
         <div className="about-info mb-5">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
             Café de Autor
           </h2>
+          <div className="projects container mx-auto grid md:grid-cols-3 gap-10">
+        {projects.map((project, i) => {
+          return (
+            <div className="relative" key={i}>
+              <img src={project.img} alt={project.title} />
+              <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+                <p className="py-5 text-center font-bold px-2 text-white">
+                  <p>{project.comp3}</p>
+                  {project.comp1}
+                  <p></p>
+                  {project.cristaleria}
+                </p>
+                <p>{project.comp2}</p>
+                <p>{project.comp4}</p>
+                <p>{project.comp5}</p>
+                <p>{project.comp7}</p>
+                <p>{project.comp6}</p>
+                <p>{project.comp8}</p>
+                <p>{project.comp9}</p>
+                {project.preparacion}
+
+                <div className="mx-auto">
+                  {/* <a
+                    href={project.live}
+                    className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
+                  >
+                    Live
+                  </a> */}
+                  {/* <a
+                    href={project.code}
+                    className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold"
+                  >
+                    Code
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
           <p className="pb-5">
             CB-Dream es un café de autor único y exclusivo que combina los
             mejores granos de café de la región sur occidente antioqueño con los
@@ -102,46 +143,7 @@ const Cafe = () => {
         <div className="about-img"></div>
       </div>
 
-      <div className="projects container mx-auto grid md:grid-cols-3 gap-10">
-        {projects.map((project, i) => {
-          return (
-            <div className="relative" key={i}>
-              <img src={project.img} alt={project.title} />
-              <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
-                <p className="py-5 text-center font-bold px-2 text-white">
-                  <p>{project.comp3}</p>
-                  {project.comp1}
-                  <p></p>
-                  {project.cristaleria}
-                </p>
-                <p>{project.comp2}</p>
-                <p>{project.comp4}</p>
-                <p>{project.comp5}</p>
-                <p>{project.comp7}</p>
-                <p>{project.comp6}</p>
-                <p>{project.comp8}</p>
-                <p>{project.comp9}</p>
-                {project.preparacion}
-
-                <div className="mx-auto">
-                  {/* <a
-                    href={project.live}
-                    className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
-                  >
-                    Live
-                  </a> */}
-                  {/* <a
-                    href={project.code}
-                    className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold"
-                  >
-                    Code
-                  </a> */}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      
     </section>
   );
 };
