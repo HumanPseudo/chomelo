495e858eb9f59579b90a6b42d7f4596e5b6b9840
David Lasso <camilolasso@trascenderglobal.com>
2023-04-13 15:20:17 -0500

    cafe de autor

--- src/page/Home.js
+++ src/page/Home.js
@@ -4,6 +4,7 @@ import About from "../components/About";
 import Projects from "../components/Projects";
 import Blog from "../components/Blog";
 import Contact from "../components/Contact";
+import Cafe from "../components/Cafe";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <About />
       <Projects />
+      <Cafe />
       {/* <Blog /> */}
       <Contact />
     </>
